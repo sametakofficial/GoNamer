7d797e05df9d2e4eaeffe24134793f789905c5f5
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-09-21 21:57:57 +0200

    feat: Add TvShow struct and interface
    This commit adds the `TvShow` struct and `TvShowClient` interface to support TV show data retrieval. The `TvShow` struct includes fields for ID, title, overview, first air date, and poster URL. The `TvShowClient` interface includes methods for searching TV shows and getting details of a specific TV show by ID.

--- internal/mediadata/interfaces.go
+++ internal/mediadata/interfaces.go
@@ -5,6 +5,7 @@ type Movie struct {
 	Title       string `json:"title"`
 	Overview    string `json:"overview"`
 	ReleaseDate string `json:"release_date"`
+	PosterURL   string `json:"poster_url"`
 }
 
 type MovieResults struct {
@@ -13,7 +14,26 @@ type MovieResults struct {
 	ResultsPerPage int64   `json:"results_per_page"`
 }
 
+type TvShow struct {
+	ID          string `json:"id"`
+	Title       string `json:"title"`
+	Overview    string `json:"overview"`
+	FistAirDate string `json:"first_air_date"`
+	PosterURL   string `json:"poster_url"`
+}
+
+type TvShowResults struct {
+	TvShows        []TvShow `json:"tv_shows"`
+	Totals         int64    `json:"totals"`
+	ResultsPerPage int64    `json:"results_per_page"`
+}
+
 type MovieClient interface {
 	SearchMovie(query string, page int) (MovieResults, error)
-	GetMovie(id int) (Movie, error)
+	GetMovie(id string) (Movie, error)
+}
+
+type TvShowClient interface {
+	SearchTvShow(query string, page int) (TvShowResults, error)
+	GetTvShow(id string) (TvShow, error)
 }
