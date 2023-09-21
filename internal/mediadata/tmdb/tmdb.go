10cea7eb55b067501bbca0de76e48e305cfb6b2c
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-09-21 13:51:54 +0200

    feat: Add TMDB package for movie data retrieval
    This commit adds a new file `tmdb.go` to the `internal/mediadata/tmdb` directory. The file contains the implementation of the TMDB package, which provides functions for searching and retrieving movie data from the TMDB API.
    
    The `tmdb.go` file includes the following significant changes:
    - Defines types and functions related to TMDB client configuration
    - Implements functions for setting language and adult options
    - Implements a function for creating a default configuration with an API key
    - Defines a struct `tmdbClient` that holds the TMDB client and configuration options
    - Implements functions for searching movies and getting movie details using the TMDB client
    - Implements helper functions for building movie objects from search results and movie details
    
    These changes enable users to interact with the TMDB API to retrieve movie data based on search queries or specific movie IDs.

---
+++ internal/mediadata/tmdb/tmdb.go
@@ -0,0 +1,110 @@
+package tmdb
+
+import (
+	"github.com/cyruzin/golang-tmdb"
+	"github.com/nouuu/mediatracker/internal/mediadata"
+	"log/slog"
+	"strconv"
+)
+
+type OptFunc func(opts *Opts)
+
+type AllOpts struct {
+	APIKey string
+	Opts
+}
+
+type Opts struct {
+	Lang  string
+	Adult bool
+}
+
+func WithLang(lang string) OptFunc {
+	return func(opts *Opts) {
+		opts.Lang = lang
+	}
+}
+
+func WithAdult(adult bool) OptFunc {
+	return func(opts *Opts) {
+		opts.Adult = adult
+	}
+}
+
+func defaultOpts(apiKey string) AllOpts {
+	return AllOpts{
+		APIKey: apiKey,
+		Opts: Opts{
+			Lang:  "en-US",
+			Adult: false,
+		},
+	}
+}
+
+type tmdbClient struct {
+	client *tmdb.Client
+	opts   AllOpts
+}
+
+func cfgMap(cfg AllOpts) map[string]string {
+	return map[string]string{
+		"language":      cfg.Lang,
+		"include_adult": strconv.FormatBool(cfg.Adult),
+	}
+}
+
+func NewMovieClient(APIKey string, opts ...OptFunc) mediadata.MovieClient {
+	o := defaultOpts(APIKey)
+	for _, optF := range opts {
+		optF(&o.Opts)
+	}
+
+	client, err := tmdb.Init(o.APIKey)
+	if err != nil {
+		slog.Error("Failed to initialize TMDB client", slog.Any("error", err))
+	}
+	return &tmdbClient{client: client, opts: o}
+}
+
+func (t *tmdbClient) SearchMovie(query string, page int) (mediadata.MovieResults, error) {
+	searchMovies, err := t.client.GetSearchMovies(query, cfgMap(t.opts))
+	if err != nil {
+		return mediadata.MovieResults{}, err
+	}
+	var movies []mediadata.Movie = buildMovieFromResult(searchMovies.SearchMoviesResults)
+	return mediadata.MovieResults{
+		Movies:         movies,
+		Totals:         searchMovies.TotalResults,
+		ResultsPerPage: 20,
+	}, nil
+}
+
+func (t *tmdbClient) GetMovie(id int) (mediadata.Movie, error) {
+	movieDetails, err := t.client.GetMovieDetails(id, cfgMap(t.opts))
+	if err != nil {
+		return mediadata.Movie{}, err
+	}
+	return buildMovie(movieDetails), nil
+}
+
+func buildMovieFromResult(result *tmdb.SearchMoviesResults) []mediadata.Movie {
+	var movies []mediadata.Movie = make([]mediadata.Movie, len(result.Results))
+	for i, movie := range result.Results {
+		movies[i] = buildMovie(&tmdb.MovieDetails{
+			ID:          movie.ID,
+			Title:       movie.Title,
+			Overview:    movie.Overview,
+			ReleaseDate: movie.ReleaseDate,
+		})
+	}
+	return movies
+}
+
+func buildMovie(movie *tmdb.MovieDetails) mediadata.Movie {
+	return mediadata.Movie{
+		ID:          strconv.FormatInt(movie.ID, 10),
+		Title:       movie.Title,
+		Overview:    movie.Overview,
+		ReleaseDate: movie.ReleaseDate,
+	}
+}
