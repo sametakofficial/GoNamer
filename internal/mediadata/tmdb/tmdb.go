7a0a83a30eefc593bdd9f104cb0b25f5cb55e092
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-09-21 21:58:22 +0200

    feat: Add support for TV show client
    This commit adds a new function `NewTvShowClient` to create a client for retrieving TV show data from TMDB. It also implements the `SearchTvShow` and `GetTvShow` methods in the `tmdbClient` struct. The `buildTvShowFromResult` and `buildTvShow` functions are added to convert TMDB TV show data into the internal representation.

--- internal/mediadata/tmdb/tmdb.go
+++ internal/mediadata/tmdb/tmdb.go
@@ -69,6 +69,19 @@ func NewMovieClient(APIKey string, opts ...OptFunc) mediadata.MovieClient {
 	return &tmdbClient{client: client, opts: o}
 }
 
+func NewTvShowClient(APIKey string, opts ...OptFunc) mediadata.TvShowClient {
+	o := defaultOpts(APIKey)
+	for _, optF := range opts {
+		optF(&o.Opts)
+	}
+
+	client, err := tmdb.Init(o.APIKey)
+	if err != nil {
+		slog.Error("Failed to initialize TMDB client", slog.Any("error", err))
+	}
+	return &tmdbClient{client: client, opts: o}
+}
+
 func (t *tmdbClient) SearchMovie(query string, page int) (mediadata.MovieResults, error) {
 	searchMovies, err := t.client.GetSearchMovies(query, cfgMap(t.opts))
 	if err != nil {
@@ -82,25 +95,35 @@ func (t *tmdbClient) SearchMovie(query string, page int) (mediadata.MovieResults
 	}, nil
 }
 
-func (t *tmdbClient) GetMovie(id int) (mediadata.Movie, error) {
-	movieDetails, err := t.client.GetMovieDetails(id, cfgMap(t.opts))
+func (t *tmdbClient) GetMovie(id string) (mediadata.Movie, error) {
+	var idInt int
+	idInt, err := strconv.Atoi(id)
+	if err != nil {
+		return mediadata.Movie{}, err
+	}
+	movieDetails, err := t.client.GetMovieDetails(idInt, cfgMap(t.opts))
 	if err != nil {
 		return mediadata.Movie{}, err
 	}
 	return buildMovie(movieDetails), nil
 }
 
-func buildMovieFromResult(result *tmdb.SearchMoviesResults) []mediadata.Movie {
-	var movies []mediadata.Movie = make([]mediadata.Movie, len(result.Results))
-	for i, movie := range result.Results {
-		movies[i] = buildMovie(&tmdb.MovieDetails{
-			ID:          movie.ID,
-			Title:       movie.Title,
-			Overview:    movie.Overview,
-			ReleaseDate: movie.ReleaseDate,
-		})
+func (t *tmdbClient) SearchTvShow(query string, page int) (mediadata.TvShowResults, error) {
+	searchTvShows, err := t.client.GetSearchTVShow(query, cfgMap(t.opts))
+	if err != nil {
+		return mediadata.TvShowResults{}, err
 	}
-	return movies
+	var tvShows []mediadata.TvShow = buildTvShowFromResult(searchTvShows.SearchTVShowsResults)
+	return mediadata.TvShowResults{
+		TvShows:        tvShows,
+		Totals:         searchTvShows.TotalResults,
+		ResultsPerPage: 20,
+	}, nil
+}
+
+func (t *tmdbClient) GetTvShow(id string) (mediadata.TvShow, error) {
+	//TODO implement me
+	panic("implement me")
 }
 
 func buildMovie(movie *tmdb.MovieDetails) mediadata.Movie {
@@ -109,5 +132,44 @@ func buildMovie(movie *tmdb.MovieDetails) mediadata.Movie {
 		Title:       movie.Title,
 		Overview:    movie.Overview,
 		ReleaseDate: movie.ReleaseDate,
+		PosterURL:   path.Join(tmdbImageBaseUrl, movie.PosterPath),
 	}
+}
+
+func buildMovieFromResult(result *tmdb.SearchMoviesResults) []mediadata.Movie {
+	var movies = make([]mediadata.Movie, len(result.Results))
+	for i, movie := range result.Results {
+		movies[i] = buildMovie(&tmdb.MovieDetails{
+			ID:          movie.ID,
+			Title:       movie.Title,
+			Overview:    movie.Overview,
+			ReleaseDate: movie.ReleaseDate,
+			PosterPath:  movie.PosterPath,
+		})
+	}
+	return movies
+}
+
+func buildTvShow(tvShow *tmdb.TVDetails) mediadata.TvShow {
+	return mediadata.TvShow{
+		ID:          strconv.FormatInt(tvShow.ID, 10),
+		Title:       tvShow.Name,
+		Overview:    tvShow.Overview,
+		FistAirDate: tvShow.FirstAirDate,
+		PosterURL:   path.Join(tmdbImageBaseUrl, tvShow.PosterPath),
+	}
+}
+
+func buildTvShowFromResult(result *tmdb.SearchTVShowsResults) []mediadata.TvShow {
+	var tvShows = make([]mediadata.TvShow, len(result.Results))
+	for i, tvShow := range result.Results {
+		tvShows[i] = buildTvShow(&tmdb.TVDetails{
+			ID:           tvShow.ID,
+			Name:         tvShow.Name,
+			Overview:     tvShow.Overview,
+			FirstAirDate: tvShow.FirstAirDate,
+			PosterPath:   tvShow.PosterPath,
+		})
+	}
+	return tvShows
 }
