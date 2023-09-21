a7f22f44688e507e37eaad44b50d74f62d5aef73
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-09-21 21:58:31 +0200

    feat: Add TV show search functionality
    This commit adds the ability to search for TV shows using the TMDB API. The `main` function now includes a call to `tvShowClient.SearchTvShow` with the query "Bleach" and displays the results using the new `showTvShowResults` function.

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/nouuu/mediatracker/conf"
+	"github.com/nouuu/mediatracker/internal/mediadata"
 	"github.com/nouuu/mediatracker/internal/mediadata/tmdb"
 	"log/slog"
 	"os"
@@ -11,17 +12,48 @@ import (
 
 func main() {
 	config := conf.LoadConfig()
-	movieClient := tmdb.NewMovieClient(config.TMDBAPIKey, tmdb.WithLang("fr-FR"))
-	movies, err := movieClient.SearchMovie("The matrix", 1)
+	movieClient := tmdb.NewMovieClient(
+		config.TMDBAPIKey,
+		tmdb.WithLang("fr-FR"),
+		tmdb.WithAdult(false),
+	)
+	tvShowClient := tmdb.NewTvShowClient(
+		config.TMDBAPIKey,
+		tmdb.WithLang("fr-FR"),
+		tmdb.WithAdult(false),
+	)
+	movies, err := movieClient.SearchMovie("Tarzan", 1)
 	if err != nil {
 		slog.Error("Failed to search movie", slog.Any("error", err))
 		os.Exit(1)
 	}
+	showMovieResults(movies)
+	tvShows, err := tvShowClient.SearchTvShow("Bleach", 1)
+	if err != nil {
+		slog.Error("Failed to search tv show", slog.Any("error", err))
+		os.Exit(1)
+	}
+	showTvShowResults(tvShows)
+}
+
+func showMovieResults(movies mediadata.MovieResults) {
 	slog.Info("Movies")
 	for _, movie := range movies.Movies {
 		mJson, err := json.MarshalIndent(movie, "", "  ")
 		if err != nil {
 			slog.Error("Failed to marshal movie", slog.Any("error", err))
+			os.Exit(1)
+		}
+		fmt.Println(string(mJson))
+	}
+}
+
+func showTvShowResults(tvShows mediadata.TvShowResults) {
+	slog.Info("TvShows")
+	for _, tvShow := range tvShows.TvShows {
+		mJson, err := json.MarshalIndent(tvShow, "", "  ")
+		if err != nil {
+			slog.Error("Failed to marshal tv show", slog.Any("error", err))
 			os.Exit(1)
 		}
 		fmt.Println(string(mJson))
